2480fbaee67747eb8a6d5dd5ae99b96bcf74ff9a
Sir Charly <capereiro@gmail.com>
2020-08-29 17:36:05 -0300

    Code cleaning II

--- create.py
+++ create.py
@@ -1,9 +1,9 @@
 import os
 import sys # Para manejo de I/O
-import webbrowser # Para manejo de browser
-from selenium import webdriver
 from dotenv import load_dotenv
 from github import Github # API github
+# import webbrowser # Para manejo de browser
+# from selenium import webdriver
 
 
 ##### Variable loading process #####
@@ -16,25 +16,16 @@ project_path = os.getenv("FILEPATH")
 driver_path = os.getenv("DRIVER_PATH")
 ##### End variable loading #####
 
-browser = webdriver.Chrome(executable_path=driver_path)
-
 def create():
     project_name = str(sys.argv[1])
-    # browser = webbrowser.get(browserName)
     
-    browser.get(url)
     user = Github(usr, psw).get_user()
     repo = user.create_repo(project_name)
-    ##### Folder management #####
-    # os.mkdir(project_path + folderName)
-
-    print('-----------------------------------')
+  
+    print('----------------------------------------------------------------')
     print ("Welcome to setup folders for Python projects!")
-    # url = "http://" + str(sys.argv[2])
-    print('-----------------------------------')
-    print('Path project: ' + project_path)
-    print('Creating project folder: ' + project_name + ' Ok')
-    print('-----------------------------------')
+    print('----------------------------------------------------------------')
+    print("Succesfully created github repository: {}".format(project_name))
 
 
 if __name__ == "__main__":
